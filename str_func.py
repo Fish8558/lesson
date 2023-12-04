8abd63a6d0427a7c6d0467f5f7042f5f86753c4c
Stas <stas_sever@mail.ru>
2023-12-05 00:19:01 +0500

    added new_foo2

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def new_foo(value):
+    """Функция делает заглавные буквы в строке"""
     return value.upper()
+
+
+def new_foo2(value):
+    """Функция делает заглавные буквы в каждом слове"""
+    return value.title()
